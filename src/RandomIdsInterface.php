8f72021a0a9cc7b71763bbd0f45573b28f0af9a7
will <will@cn09.com>
2019-09-10 17:17:17 +0800

    temp

---
+++ src/RandomIdsInterface.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author Will Lin <will@cn09.com>
+ */
+
+interface RandomIdsInterface
+{
+    /**
+     * This function is the main function,it will pop an ID from the random store
+     * @param int $lastId if your random store accidentally lost, library will create a new high-level storage store,all
+     * new IDs will much larger than the $lastId
+     * @return integer ID
+     */
+    public function getId(int $lastId);
+
+
+    /**
+     * Set the number of ID generated at a time
+     * @param int $limit
+     */
+    public function setLimit(int $limit);
+
+
+    /**
+     * Create a random IDs array and save it
+     *
+     * @param int $start
+     * @return mixed
+     */
+    function create(int $start);
+
+    /**
+     * Save IDs
+     * @param array $ids
+     * @return mixed
+     */
+    function save(array $ids);
+
+    /**
+     * pop an ID from the random store
+     * @param int $lastId if your random store accidentally lost, library will create a new high-level storage store,all
+     * new IDs will much larger than the $lastId
+     * @return integer ID
+     */
+    function pop(int $lastId);
+}
